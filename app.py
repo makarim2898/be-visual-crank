5630e84784f17dc1629087956562c761d5f0a17f
kaizen-epd <kaizen.epd.norepy@gmail.com>
2024-07-26 20:51:36 +0700

    register Home_page_trust_bearing

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
 from settings_function import settings
+from Home_page_trust_bearing import home_bearing
 
 app = Flask(__name__)
 CORS(app)
 app.register_blueprint(settings)
+app.register_blueprint(home_bearing)
 
 @app.route('/')
 def index():
-    return "This is a basic flask application"
+    return "Badan anda bau, Harap mandi Wajib"
 
 if __name__ == '__main__':
     app.run(debug=True)
