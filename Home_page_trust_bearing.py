f670e16e0e3d5b1a2febd1dc22a075c976baca73
kaizen-epd <kaizen.epd.norepy@gmail.com>
2024-07-29 09:31:12 +0700

    penambahan fuction save image penambahan function trigger inspection

--- Home_page_trust_bearing.py
+++ Home_page_trust_bearing.py
@@ -6,12 +6,17 @@ import pandas as pd
 import datetime
 import numpy as np
 import time
+from ultralytics import YOLO
 
 home_bearing = Blueprint('bearing_routes', __name__)
 CORS(home_bearing)
 
+inspectionFlag = False
+bearing_detected = False
+
+model = YOLO("./models/yolov8m.pt")
 def stream_video(device):
-    global latest_frame
+    global latest_frame, bearing_detected, inspectionFlag
     time.sleep(2)
     cap = cv2.VideoCapture(device)
     
@@ -42,16 +47,38 @@ def stream_video(device):
         if not ret:
             print("Tidak dapat membaca frame")
             break
+        results = model(frame, conf=0.9, classes=0)
+        annotated_frame = results[0].plot()
         
+        #jika trigger untuk deteksi on
+        if inspectionFlag:
+        # logika untuk mendapatkan data object yang di deteksi
+        #kemudian perbarui nilai di global variabel bearing_detected
+            for r in results:
+                detected_object = len(r.boxes.cls)
+                if detected_object:
+                    bearing_detected = True
+                    save_image(annotated_frame, 'OKE', 'Deteksi_oke')
+                    print(f'Detected object: {detected_object}')
+                    inspectionFlag = False
+                else:
+                    bearing_detected = False
+                    print('No bearing object detected')
+                    save_image(annotated_frame, 'NG', 'Tidak_terdeteksi')
+                    print(f'Detected object: {detected_object}')
+                    inspectionFlag = False
+            
+                    
+        # dibawah ini logika untuk memperkecil ukuran frame agar ringan saat di show up
         #Set frame width and height for 16:9 aspect ratio and 1080p resolution
         frame_width = 1280
         frame_height = 720  # Initial frame height for 16:9 aspect ratio and 720p resolution
 
         # Calculate the frame width based on the aspect ratio
         frame_width = int((frame_height / 9) * 16)
-        frame = cv2.resize(frame, (int(frame_width * (810 / frame_height)), 810))
+        annotated_frame = cv2.resize(annotated_frame, (int(frame_width * (810 / frame_height)), 810))
         latest_frame = frame
-        ret, buffer = cv2.imencode('.jpg', frame)
+        ret, buffer = cv2.imencode('.jpg', annotated_frame)
         frame = buffer.tobytes()
 
         yield (b'--frame\r\n'
@@ -59,6 +86,30 @@ def stream_video(device):
 
     cap.release()
     cv2.destroyAllWindows()
+    
+def start_inspection():
+    global inspectionFlag
+    inspectionFlag = True
+    return 
+
+def save_image(images_to_save, raw_file_name, image_category):
+    corrected_name = raw_file_name.replace(' ', '_')
+
+    # Get current date and time for saving the file name.
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    file_name = f"{corrected_name}_{timestamp}.jpg"
+    
+    # Buat Direktory download
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    downloads_directory = os.path.join(current_directory, f'Downloads/{image_category}')
+    os.makedirs(downloads_directory, exist_ok=True)
+    
+    #simpan ke direktori download
+    image_path = os.path.join(downloads_directory, f"{file_name}.png")  # Menambahkan timestamp pada nama file
+    
+    cv2.imwrite(image_path, images_to_save)
+    print(f"Gambar disimpan di {image_path}")
+    
     
 @home_bearing.route('/bearing/show-video', methods=['GET'])
 def settings_show_video():
@@ -68,4 +119,11 @@ def settings_show_video():
 
 @home_bearing.route('/bearing/get-data', methods=['GET'])
 def get_data():
-    return "sucess masuk"
+    global bearing_detected
+    data = {'bearing_detected': bearing_detected}
+    return jsonify(data)
+
+@home_bearing.route('/bearing/start', methods=['GET'])
+def startInspection():
+    start_inspection()
+    return "sucess startingspection"
